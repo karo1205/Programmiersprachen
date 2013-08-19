055359c60257832a80c9e849d927f14caa684ef2
Tomschi <codingtomschi@gmail.com>
2013-08-19 17:37:43 +0200

    add subroutine to parse parameters and replace it with the string values in the conf file

--- aufgabe_2/aufgabe_2.pl
+++ aufgabe_2/aufgabe_2.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 
 my $confFile;
+my %parameters = ();
 
 sub proccedArguments() {
   if(@ARGV > 0 && $ARGV[0] =~ /conf$/) {
@@ -20,41 +21,77 @@ sub openFile() {
     die $!;
   }
 
-  while (<FILE>) { &evalInput($_) }
+  while (<FILE>) {
+    &evalInput($_);
+  }
   
   close(FILE);
 }
 
-sub evalInput() {
+sub evalInput(@) {
   my @split = split(' ', $_[0]);
   
   if(defined $split[0]) {
     if($split[0] =~ /^var$/) {
       &executeParam(@split);
+    } elsif($split[0] =~ /^alternative:$/) {
+      
+    } elsif($split[0] =~ /^sequence:$/) {
+      
     } else {
       &executeCmd(@split);
     }
   }
-  
-  
 }
 
-sub executeParam() {
-  
+sub executeParam(@) {
+  $parameters{$_[1]} = $_[3];
 }
 
-sub executeCmd() {
-  foreach my $value (@_) {
-    my $status = system($value);
-    return $status;
+sub replaceWithParam(@) {
+  foreach my $key (keys(%parameters)) {
+    if($key eq $_[0]) {
+      return $parameters{$key};
+    } else {
+      return $_;
+    }
   }
+}
+
+sub executeCmd(@) {
+  my $index = 0;
+  my $command;
+  my @param;
+  my $value;
+  foreach $value (@_) {
+    if($index eq 0) {
+      $command = $value;
+      $index++;
+    } else {
+      my $p = &replaceWithParam($value);
+      print $p;
+      push(@param, $p);
+      $index++;
+    }
+  }
+  
+  foreach $value (@param) {
+    $command = $command . " " . $value;
+  }
+  
+  my $result = `$command 2>&1`;
+    if($? < 0) {
+      print "Executing command failed\n";
+    } else {
+      print $result;
+      return $0;
+    }
    
 }
 
 &proccedArguments();
 &openFile();
-#&parseFile();
 
-foreach my $arg (@ARGV) {
-    print $arg, "\n";
-}
+# foreach my $arg (@ARGV) {
+#     print $arg, "\n";
+# }
